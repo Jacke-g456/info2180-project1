0058b9f967da310066f3ff999dc807900fdcb3aa
Jacke-g456 <steviecool2014@gmail.com>
2025-11-08 18:54:40 -0500

    Javascript Implementation

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function (){
+const form = document.querySelector("form"); 
+const input = document.getElementById("email");
+const output = document.querySelector(".message");
+form.addEventListener("submit", function(event){
+    event.preventDefault();  // stops the form submission from reloading the page
+    const email = input.value; // collects the entered email
+    var message='';
+    if (email==''){
+         message = "Please enter a valid email address.";
+    }
+    else{
+         message = `Thank you! Your email address '${email}' has been added to our mailing list`;
+    }
+    
+    output.textContent = message; //places message in the div
+
+    console.log(message);
+
+});
+
+});
